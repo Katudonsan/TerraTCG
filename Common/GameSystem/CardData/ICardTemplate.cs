b7f5bc33c142058b93dd359777b6f214f66e7843
かつ丼さん <109509123+Katudonsan@users.noreply.github.com>
2026-05-26 17:55:29 +0900

    internal -> public
    変更part1
    しかし、芋づる式にエラーが出るのでまだまだpublic化は続く。

--- Common/GameSystem/CardData/ICardTemplate.cs
+++ Common/GameSystem/CardData/ICardTemplate.cs
@@ -1,49 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
 using TerraTCG.Common.GameSystem.GameState;
 using TerraTCG.Content.Gores;
 
 namespace TerraTCG.Common.GameSystem.CardData
 {
-    internal interface ICardTemplate
-    {
-        public Card Card { get; }
-        public Card CreateCard();
-    }
+	public interface ICardTemplate
+	{
+		Card Card { get; }
+		Card CreateCard();
+	}
 
 
-    internal abstract class BaseCardTemplate : ModSystem, ICardTemplate
-    {
-        private Card _card;
-        public Card Card { 
-            get
-            {
-                bool newCard = _card == null;
-                _card ??= CreateCard();
-                if (newCard)
-                {
+	public abstract class BaseCardTemplate : ModSystem, ICardTemplate
+	{
+		private Card _card;
+		public Card Card
+		{
+			get
+			{
+				bool newCard = _card == null;
+				_card ??= CreateCard();
+				if (newCard)
+				{
 					if (!Main.dedServ)
 					{
 						var goreInstance = new CardGore(_card.Name, _card.TexturePath);
 						Mod.AddContent(goreInstance);
 						_card.GoreType = goreInstance.Type;
 					}
-                }
-                return _card;
-            } 
-        }
+				}
+				return _card;
+			}
+		}
 
-        public abstract Card CreateCard();
+		public abstract Card CreateCard();
 
-        public override void OnModLoad()
-        {
+		public override void OnModLoad()
+		{
 			// Guarantee that it loads on time
-            _ = Card;
-        }
-    }
+			_ = Card;
+		}
+	}
 }
--- Common/GameSystem/GameState/Attack.cs
+++ Common/GameSystem/GameState/Attack.cs
@@ -1,64 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TerraTCG.Common.GameSystem.GameState.Modifiers;
 
 namespace TerraTCG.Common.GameSystem.GameState
 {
-    internal delegate void DoAttack(Attack attack, Zone sourceZone, Zone targetZone);
+	public delegate void DoAttack(Attack attack, Zone sourceZone, Zone targetZone);
 
-    internal delegate List<ICardModifier> ApplyModifiers(Zone appliedZone);
-    internal struct Attack()
-    {
-        internal string Name { get; set; }
-        internal string Description { get; set; }
-        internal int Damage { get; set; }
-        internal int SelfDamage { get; set; }
-        internal int Cost { get; set; }
+	public delegate List<ICardModifier> ApplyModifiers(Zone appliedZone);
+	public struct Attack()
+	{
+		public string Name { get; set; }
+		internal string Description { get; set; }
+		public int Damage { get; set; }
+		internal int SelfDamage { get; set; }
+		public int Cost { get; set; }
 
-        internal DoAttack DoAttack { get; set; } = DefaultAttack;
+		internal DoAttack DoAttack { get; set; } = DefaultAttack;
 
-        // Modifiers applied to the attacker after the attack goes through
-        internal ApplyModifiers SourceModifiers { get; set; }
+		// Modifiers applied to the attacker after the attack goes through
+		internal ApplyModifiers SourceModifiers { get; set; }
 
-        // Modifiers applied to the target after the attack goes through
-        internal ApplyModifiers TargetModifiers { get; set; }
+		// Modifiers applied to the target after the attack goes through
+		internal ApplyModifiers TargetModifiers { get; set; }
 
 
-        // How much damage this attack does per mana spent, used by bots
-        // in decision sequencing
-        internal readonly float ManaEfficiency => Damage / (float)(Math.Max(Cost, 1));
+		// How much damage this attack does per mana spent, used by bots
+		// in decision sequencing
+		internal readonly float ManaEfficiency => Damage / (float)(Math.Max(Cost, 1));
 
-        internal static void DefaultAttack(Attack attack, Zone sourceZone, Zone targetZone)
-        {
-            targetZone.PlacedCard.CurrentHealth -= attack.Damage;
-            if(attack.SelfDamage >= 0)
-            {
-                sourceZone.PlacedCard.CurrentHealth -= attack.SelfDamage;
-            } else
-            {
-                sourceZone.PlacedCard.Heal(-attack.SelfDamage);
-            }
+		internal static void DefaultAttack(Attack attack, Zone sourceZone, Zone targetZone)
+		{
+			targetZone.PlacedCard.CurrentHealth -= attack.Damage;
+			if (attack.SelfDamage >= 0)
+			{
+				sourceZone.PlacedCard.CurrentHealth -= attack.SelfDamage;
+			}
+			else
+			{
+				sourceZone.PlacedCard.Heal(-attack.SelfDamage);
+			}
 
-            sourceZone.PlacedCard.CardModifiers.AddRange([.. attack.SourceModifiers?.Invoke(sourceZone) ?? []]);
-            targetZone.PlacedCard.CardModifiers.AddRange([.. attack.TargetModifiers?.Invoke(targetZone) ?? []]);
-        }
+			sourceZone.PlacedCard.CardModifiers.AddRange([.. attack.SourceModifiers?.Invoke(sourceZone) ?? []]);
+			targetZone.PlacedCard.CardModifiers.AddRange([.. attack.TargetModifiers?.Invoke(targetZone) ?? []]);
+		}
 
-        internal Attack Copy()
-        {
-            return new Attack() 
-            {
-                Name = Name,
-                Description = Description,
-                Damage = Damage,
-                SelfDamage = SelfDamage,
-                Cost = Cost,
-                SourceModifiers = SourceModifiers,
-                TargetModifiers = TargetModifiers,
-                DoAttack = DoAttack
-            };
-        }
-    }
+		internal Attack Copy()
+		{
+			return new Attack()
+			{
+				Name = Name,
+				Description = Description,
+				Damage = Damage,
+				SelfDamage = SelfDamage,
+				Cost = Cost,
+				SourceModifiers = SourceModifiers,
+				TargetModifiers = TargetModifiers,
+				DoAttack = DoAttack
+			};
+		}
+	}
 }
